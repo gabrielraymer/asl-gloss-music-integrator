d40a0ce3f06ae7fc3c6624fe300102bf8fe9a168
gabrielraymer <gabriel.raymer@outlook.com>
2025-06-05 18:00:43 -0600

    Deleted _layout.tsx

--- app/(modals)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from 'expo-router';
-
-export default function ModalLayout() {
-  return (
-    <Stack screenOptions={{ presentation: 'modal' }}>
-      <Stack.Screen name="import" options={{ title: 'Import Song' }} />
-    </Stack>
-  );
-}
